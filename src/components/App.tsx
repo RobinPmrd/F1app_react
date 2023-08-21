32788b13a6f9313532b6bd0a6fb1ee415de8f14d
RobinPmrd <robinpmrd@gmail.com>
2023-08-21 19:30:54 +0200

    Change to Typecript

---
+++ src/components/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useState } from 'react';
+import '../styles/App.css';
+import Drivers from './Drivers';
+import Header from './Header';
+import SideMenu from './SideMenu';
+
+function App() {
+  const [page, setPage] = useState<string>("home");
+  return (
+      <div className='body'>
+        <Header />
+        <main>
+          <SideMenu setPage={setPage}/>
+          {page === "Drivers" && <Drivers />}
+        </main>
+      </div>
+  )
+  }
+
+export default App;
